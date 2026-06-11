8321014be91692c7a4c723cbf2ee33a62a67059a
caiobreno123 <caiobrenopiresduarte@gmail.com>
2026-06-11 10:22:36 -0300

    Delete src directory

--- src/services/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL='COLE_SUA_URL';
-const SUPABASE_ANON_KEY='COLE_SUA_ANON_KEY';
-
-export const supabase=createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
